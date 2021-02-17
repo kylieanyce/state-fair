119728bdf249e2466db838cd9e695d89afb42577
kylieanyce <kylieanyce@gmail.com>
2021-02-17 15:09:36 -0600

    added sideshow ticket holder function, created event listener, renders to DOM

---
+++ scripts/sideshows/SideshowTicketHolder.js
@@ -0,0 +1,11 @@
+const contentTarget = document.querySelector(".sideshow")
+const eventHub = document.querySelector("#state-fair")
+
+
+export const SideshowTicketHolder = () => {
+    eventHub.addEventListener("sideshowTicketPurchased", customEvent => {
+    contentTarget.innerHTML += `
+        <div class="gawker person"></div>
+        `
+    })
+}
